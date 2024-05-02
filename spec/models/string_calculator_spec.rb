e5ec6546a3c96cc2772fdbc325b14afc88a84396
Sweta Das <sbdas1099@gmail.com>
2024-05-02 15:48:04 +0530

    [commit#3 test case for empty string and number]

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe StringCalculator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe StringCalculator do
+  describe '#Add' do
+    context 'when given an empty string' do
+      it 'returns 0' do
+        expect(StringCalculator.Add("")).to eq(0)
+      end
+    end
+
+    context 'when given a single number' do
+      it 'returns the number' do
+        expect(StringCalculator.Add("1")).to eq(1)
+      end
+    end   
+  end
 end
